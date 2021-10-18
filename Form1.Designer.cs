49a30a9f71d502ecbe7f2162098e47779d188d18
rehankhan-optbyte <khnrehann@gmail.com>
2021-10-18 21:12:35 +0530

    All files added to repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace CompressPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxPdfFiles = new System.Windows.Forms.ListBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Compress = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxPdfFiles
+            // 
+            this.listBoxPdfFiles.FormattingEnabled = true;
+            this.listBoxPdfFiles.ItemHeight = 15;
+            this.listBoxPdfFiles.Location = new System.Drawing.Point(30, 90);
+            this.listBoxPdfFiles.Name = "listBoxPdfFiles";
+            this.listBoxPdfFiles.Size = new System.Drawing.Size(736, 199);
+            this.listBoxPdfFiles.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(691, 61);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Compress
+            // 
+            this.Compress.Location = new System.Drawing.Point(30, 296);
+            this.Compress.Name = "Compress";
+            this.Compress.Size = new System.Drawing.Size(75, 23);
+            this.Compress.TabIndex = 2;
+            this.Compress.Text = "Compress";
+            this.Compress.UseVisualStyleBackColor = true;
+            this.Compress.Click += new System.EventHandler(this.Compress_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(125, 296);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(641, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pdf Files";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "screen",
+            "ebook",
+            "printer"});
+            this.comboBox1.Location = new System.Drawing.Point(125, 343);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Quality";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Compress);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.listBoxPdfFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxPdfFiles;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Compress;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompressPdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Select your PDF files";
+            openFileDialog.Filter = "PDF Files (*.PDF)| *.PDF";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var item in openFileDialog.FileNames)
+                {
+                    listBoxPdfFiles.Items.Add(item);
+                }
+            }
+        }
+
+        private bool CompressPDF(string InputFile, string OutPutFile, string CompressValue)
+        {
+            try
+            {
+                Process proc = new Process();
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.CreateNoWindow = true;
+                psi.ErrorDialog = false;
+                psi.UseShellExecute = false;
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                psi.FileName = string.Concat(Path.GetDirectoryName(Application.ExecutablePath), "\\ghsot.exe");
+
+
+                string args = "-sDEVICE=pdfwrite -dCompatibilityLevel=1.4" + " -dPDFSETTINGS=/" + CompressValue + " -dNOPAUSE  -dQUIET -dBATCH" + " -sOutputFile=\"" + OutPutFile + "\" " + "\"" + InputFile + "\"";
+
+
+                psi.Arguments = args;
+
+
+                //start the execution
+                proc.StartInfo = psi;
+
+                proc.Start();
+                proc.WaitForExit();
+
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        string outputDir = string.Empty;
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (string item in listBoxPdfFiles.Items)
+            {
+                string output = $"{outputDir}\\{Path.GetFileNameWithoutExtension(item)}_Compress.pdf";
+
+                string quality = string.Empty;
+                if (comboBox1.InvokeRequired)
+                {
+                    comboBox1.Invoke(new Action(()=> quality = comboBox1.Text)); 
+                }
+                else
+                {
+                    quality = comboBox1.Text;
+                }
+
+                CompressPDF(item, output, quality);
+
+                if (progressBar1.InvokeRequired)
+                {
+                    progressBar1.Invoke(new Action(() => progressBar1.Increment(1)));
+                }
+                else
+                {
+                    progressBar1.Increment(1);
+                }
+            }
+
+            
+        }
+
+        private void Compress_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputDir = folderBrowserDialog.SelectedPath;
+            }
+            progressBar1.Maximum = listBoxPdfFiles.Items.Count;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Done");
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
